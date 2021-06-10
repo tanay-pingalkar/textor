70ad0da12378cb00cbcd31a15e344ef94968a0c0
tanay-pingalkar <tpingalkar@gmail.com>
2021-06-10 22:35:47 +0530

    endpoint env

--- server/src/server.ts
+++ server/src/server.ts
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 5000;
   server.applyMiddleware({
     app,
     cors: {
-      origin: ["http://localhost:3000"],
+      origin: ["http://localhost:3000", "https://textor.vercel.app/"],
       credentials: true,
     },
   });
