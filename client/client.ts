66cb7e5be92e0ad02f85badc3860d610bbd688b8
tanay-pingalkar <tpingalkar@gmail.com>
2021-06-11 15:10:30 +0530

    endpoint env

--- client/client.ts
+++ client/client.ts
@@ -1,11 +1,8 @@
-import { gql, GraphQLClient } from "graphql-request";
+import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./generated/graphql";
 
 export const client = new GraphQLClient(process.env.ENDPOINT, {
   credentials: "include",
-  headers: {
-    authorization: `${process.env.BEARER}`,
-  },
 });
 
 export const sdk = getSdk(client);
