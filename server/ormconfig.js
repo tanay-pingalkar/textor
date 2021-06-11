15a632e61d96e7ddff812ce1eb683711380fae34
tanay-pingalkar <tpingalkar@gmail.com>
2021-06-11 12:28:13 +0530

    endpoint env

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -9,6 +9,7 @@ module.exports = {
   post: process.env.DATABASE_PORT,
   synchronize: false,
   logging: false,
+  ssl: { rejectUnauthorized: false },
   migrations: ["dist/migration/**/*.js"],
   entities: ["dist/entities/**/*.js"],
   cli: { migrationsDir: "src/migration" },
