e16f2632d00742965a10670b72ca8fa687a08be7
tanay-pingalkar <tpingalkar@gmail.com>
2021-06-12 15:16:04 +0530

    cookies to cookie

--- client/pages/api/logout.ts
+++ client/pages/api/logout.ts
@@ -6,8 +6,7 @@ export default function logout(
   res: NextApiResponse
 ): void {
   if (req.method === "GET") {
-    const body = JSON.parse(req.body);
-    res.setHeader("Set-Cookie", serialize("token", body.token, { maxAge: -1 }));
+    res.setHeader("Set-Cookie", serialize("token", "", { maxAge: -1 }));
     res.status(200).json({ msg: "great" });
   } else {
     res.status(400);
