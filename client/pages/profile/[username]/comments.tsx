4cde01bb14aa913721b64fdd13e4e8788dc55f56
tanay-pingalkar <tpingalkar@gmail.com>
2021-07-18 17:58:22 +0530

    comment route seo

--- client/pages/profile/[username]/comments.tsx
+++ client/pages/profile/[username]/comments.tsx
@@ -3,43 +3,61 @@ import { sdk } from "../../../utils/client";
 import { ProfileWithCommentsQuery } from "../../../generated/graphql";
 import Comment from "../../../components/comment";
 import User from "../../../components/user";
+import Head from "next/head";
 
 const Home: React.FC<ProfileWithCommentsQuery> = ({
   profile: { msg, me, user },
 }) => {
   return (
-    <div>
-      {msg === "great" ? (
-        <>
-          {user ? (
-            <User
-              me={me}
-              name={user.name}
-              email={user.email}
-              reputation={user.reputation}
-            ></User>
-          ) : (
-            <></>
-          )}
-          <div>
-            {user.comments.map((comment) => (
-              <Comment
-                commentId={Number(comment.id)}
-                body={comment.body}
-                upvoted={comment.upvoted}
-                downvoted={comment.downvoted}
-                votes={comment.totalVotes}
-                postId={comment.post ? comment.post.id : null}
-                me={true}
-                user={user.name}
-              ></Comment>
-            ))}
-          </div>
-        </>
-      ) : (
-        <h1>user not found</h1>
-      )}
-    </div>
+    <>
+      <Head>
+        <title>
+          {user.name}({user.reputation})
+        </title>
+        <meta name="keywords" content={user.name} />
+        <meta name="author" content={user.name} />
+        <meta
+          name="description"
+          content={`profile name : ${user.name}\nreputation : ${user.reputation}\ntotal comments : ${user.comments.length}`}
+        />
+        <meta
+          name="twitter:card"
+          content={`profile name : ${user.name}\nreputation : ${user.reputation}\ntotal comments : ${user.comments.length}`}
+        ></meta>
+      </Head>
+      <div>
+        {msg === "great" ? (
+          <>
+            {user ? (
+              <User
+                me={me}
+                name={user.name}
+                email={user.email}
+                reputation={user.reputation}
+              ></User>
+            ) : (
+              <></>
+            )}
+            <div>
+              {user.comments.map((comment) => (
+                <Comment
+                  commentId={Number(comment.id)}
+                  body={comment.body}
+                  upvoted={comment.upvoted}
+                  downvoted={comment.downvoted}
+                  votes={comment.totalVotes}
+                  postId={comment.post ? comment.post.id : null}
+                  me={true}
+                  user={user.name}
+                ></Comment>
+              ))}
+            </div>
+          </>
+        ) : (
+          <h1>user not found</h1>
+        )}
+      </div>
+    </>
   );
 };
 
